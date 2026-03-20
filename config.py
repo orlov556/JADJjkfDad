99078dd74b5b89e2320224c11efc732eedbc69e7
orlov556 <orlovsacha08@gmail.com>
2026-03-20 18:38:41 +0300

    Create config.py

---
+++ config.py
@@ -0,0 +1,6 @@
+import os
+
+BOT_TOKEN = os.environ.get("BOT_TOKEN")
+ADMIN_ID = int(os.environ.get("ADMIN_ID", "5697184715"))
+WEBAPP_URL = os.environ.get("WEBAPP_URL", "https://your-app.railway.app")
+DATABASE_URL = os.environ.get("DATABASE_URL")
